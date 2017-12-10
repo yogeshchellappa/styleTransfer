2b7b1b1334a69fea552f706a80d40ec9513d4795
YOGESH-PC\yoges <yogesh20101993@gmail.com>
2017-12-10 17:27:54 -0600

    Intiial Commit

---
+++ StyleTransfer/downloadStyles.py
@@ -0,0 +1,93 @@
+import time      
+import sys    
+import os
+import urllib3
+import glob
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+from PIL import Image
+import urllib.request
+
+def downloadPage(url):
+    try:
+        headers = {}
+        headers['User-Agent'] = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+        req = urllib.request.Request(url, headers = headers)
+        resp = urllib.request.urlopen(req)
+        respData = str(resp.read())
+        return respData
+    except Exception as e:
+        print(str(e))
+
+def getNextImageItem(page,extension):
+    start_line = page.find('rg_di')
+    if start_line == -1:    #If no links are found then give an error!
+        end_quote = 0
+        link = "no_links"
+        return link, end_quote
+    else:
+        start_line = page.find('"class="rg_meta"')
+        start_content = page.find('"ou"',start_line+1)
+        end_content = page.find(',"ow"',start_content+1) 
+        content_raw = str(page[start_content+6:end_content-1])
+        if extension in content_raw:
+            return content_raw, end_content
+        else:
+            return 'skip', end_content
+
+def getAllImageItems(page, extension, numberOfImages):
+    items = []
+    while len(items) <= numberOfImages:
+        item, end_content = getNextImageItem(page,extension)
+        if item == "no_links":
+            break
+        elif item == 'skip':
+            page = page[end_content:]
+            continue
+        else:
+            items.append(item)      
+            time.sleep(0.1)        
+            page = page[end_content:]
+    return items
+
+def downloadImages(theme, extension, numberOfImages = 5, height = 300, width = 400):
+	imagePath = "Images/"
+	keywords = ['HD']
+	items = []
+	print('Theme: ' + str(theme))
+	print('Fetching styles...')
+	search = theme.replace(' ','%20')
+	
+	j = 0
+	while j<len(keywords):
+	    pure_keyword = keywords[j].replace(' ','%20')
+	    url = 'https://www.google.com/search?q=' + search + pure_keyword + '&tbm=isch'
+	    raw_html = downloadPage(url)
+	    time.sleep(0.1)
+	    items = items + (getAllImageItems(raw_html,extension,numberOfImages))
+	    j = j + 1
+
+	for i in range(numberOfImages):
+		http = urllib3.PoolManager()
+		req = http.request('GET', items[i], headers={"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
+		response = req.data
+		imageName = imagePath + "style" + str(i+1) + "." + extension
+		with open(imageName, 'wb') as file:
+			file.write(response)
+			file.flush()
+			file.close()
+			time.sleep(0.5)
+
+	# Resize images		
+	for imageName in glob.glob("Images/style*.jpg"):
+		if os.path.getsize(imageName) == 0:
+			os.remove(imageName)
+			continue
+		im = Image.open(imageName)
+		resizedImage = im.resize((height, width), Image.ANTIALIAS)
+		resizedImage.save(imageName, 'JPEG', quality=100)
+			
+		print("Completed ====> "+str(i+1))
+
+	print('Success')
+
---
+++ StyleTransfer/main.py
@@ -0,0 +1,70 @@
+import imageUtils
+import sys
+import glob
+import downloadStyles
+import os
+import styleTransfer
+
+# The VGG16-model has 13 convolutional layers.
+NUMBER_OF_CONV_LAYERS_IN_VGG16 = 13
+
+def main(contentFilePath, theme):
+    mixedStyleImage = None
+    contentImage = imageUtils.load_image(contentFilePath, max_size=None)
+    
+    content_layer_ids = [4]
+    style_layer_ids = list(range(NUMBER_OF_CONV_LAYERS_IN_VGG16))
+    
+    # Remove all previous styles
+    [os.remove(file) for file in glob.glob('Images/style*.jpg')]
+    
+    # Download style images and save them to disk.
+    # Combine styles using this network itself.
+    downloadStyles.downloadImages(theme, 'jpg', numberOfImages=3, height=600, width=600)
+    styles = glob.glob('Images/style*.jpg')
+
+    if len(styles) > 0:
+        startStyle = imageUtils.load_image(styles[0], max_size=None)
+        for image in styles[1:]:
+            styleImage = imageUtils.load_image(image, max_size=None)
+            mixedStyleImage = styleTransfer.style_transfer(content_image=startStyle,
+                                 style_image=styleImage,
+                                 content_layer_ids=content_layer_ids,
+                                 style_layer_ids=style_layer_ids,
+                                 alpha=5.0,
+                                 beta=5.0,
+                                 weight_denoise=0.3,
+                                 num_iterations=100,
+                                 step_size=2.0)
+        
+            startStyle = mixedStyleImage
+
+    imageUtils.save_image(mixedStyleImage, "Images/" + "style.jpg")
+    imageUtils.plot_image_big(mixedStyleImage)
+    
+    # Now apply on the content image
+    mixedImage = styleTransfer.style_transfer(content_image=contentImage,
+                        style_image=mixedStyleImage,
+                        content_layer_ids=content_layer_ids,
+                        style_layer_ids=style_layer_ids,
+                        alpha=1.5,
+                        beta=10.0,
+                        weight_denoise=0.3,
+                        num_iterations=100,
+                        step_size=2.0)
+
+    imageUtils.plot_images(contentImage, mixedStyleImage, mixedImage)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        content = sys.argv[1]
+        theme = sys.argv[2]
+        main(content, theme)
+    else:
+        print()
+        print("Run as:")
+        print("python " + __file__ + " <Path to content image> <theme>")
+        print()
+    
+
